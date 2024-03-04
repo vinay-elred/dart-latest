f014acdf309dae2f692d1ab4a4a2035dd4c1ef4e
Vinay_Sharma <vinay@elred.io>
2024-03-04 19:34:12 +0530

    sealed class

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:dart_latest/abstract_interface.dart';
 import 'package:dart_latest/base_class_2.dart';
 import 'package:dart_latest/final_class.dart';
 import 'package:dart_latest/mixin_class.dart';
+import 'package:dart_latest/sealed_class.dart';
 
 void main() {
   //Base Class
@@ -13,4 +14,7 @@ void main() {
   Mixin1().show("Mixin with class");
   // interface
   LoadDataFromFile().fetch();
+  //sealed
+  final val = SealedExample().exhustiveSwitch(Pen4());
+  print("sealedclass $val");
 }
